3db1d965319283814f8c31d004d71e12dfba55e4
kiranpreet867 <kkaur127@academic.rrc.ca>
2026-06-22 11:08:25 -0500

    Generate Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_12_152722) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_22_160742) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
